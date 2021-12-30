614432add80d19495bce3d5028eec1239167ed3e
Kian-Tat Lim <ktl@slac.stanford.edu>
2021-12-29 22:43:03 -0800

    Make (de)activate POSIX-compatible.

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,7 +1,7 @@
 # Syntax should be compatible with bash and zsh
-if [[ -n "$RUBIN_EUPS_PATH" ]]; then
+if [ -n "$RUBIN_EUPS_PATH" ]; then
   # push RUBIN_EUPS_PATH onto the front of EUPS_PATH if it's not already there
-  if [[ -z "$EUPS_PATH" ]]; then
+  if [ -z "$EUPS_PATH" ]; then
     export EUPS_PATH="${RUBIN_EUPS_PATH}"
   else
     export CONDA_RUBINENV_BACKUP_EUPS_PATH="${EUPS_PATH}"
